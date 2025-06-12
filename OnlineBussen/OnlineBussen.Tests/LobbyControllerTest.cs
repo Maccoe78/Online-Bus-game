0b877cbb2312e0aae2b0c21178277b4bda5f15ea
Maccoe78 <macfontys@gmail.com>
2025-06-12 10:40:40 +0200

    Made 2 Unit test for LobbyController

---
+++ OnlineBussen/OnlineBussen.Tests/LobbyControllerTest.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Moq;
+using Xunit;
+using OnlineBussen.Logic.Interfaces;
+using OnlineBussen.Logic.Controllers;
+using OnlineBussen.Logic.Models;
+
+namespace OnlineBussen.Tests
+{
+    public class LobbyControllerTest
+    {
+        private readonly Mock<ILobbyService> _mockLobbyService;
+        private readonly LobbyController _controller;
+
+        public LobbyControllerTest()
+        {
+            _mockLobbyService = new Mock<ILobbyService>();
+            _controller = new LobbyController(_mockLobbyService.Object);
+        }
+
+        [Fact]
+        public async Task CreateLobby_ReturnsSucces()
+        {
+
+            _mockLobbyService
+                .Setup(x => x.LobbyNameExistsAsync("existinglobby"))
+                .ReturnsAsync(true);
+
+            _mockLobbyService
+                .Setup(x => x.LobbyNameExistsAsync("nonlobby"))
+                .ReturnsAsync(false);
+
+
+            var result = await _controller.CreateLobbyAsync("nonlobby", "lobbypass", "username");
+
+            Assert.True(result.success);
+            Assert.Equal("Lobby created successfully", result.message);
+        }
+
+        [Fact]
+        public async Task CreateLobby_ReturnFalse_ExistingLobbyName()
+        {
+            _mockLobbyService
+                .Setup(x => x.LobbyNameExistsAsync("existinglobby"))
+                .ReturnsAsync(true);
+            _mockLobbyService
+                .Setup(x => x.LobbyNameExistsAsync("nonlobby"))
+                .ReturnsAsync(false);
+
+            var result = await _controller.CreateLobbyAsync("existinglobby", "lobbypass", "username");
+
+            Assert.False(result.success);
+            Assert.Equal("A lobby with this name already exists", result.message);
+        }
+
+    }
+}
--- OnlineBussen/OnlineBussen.Tests/UserControllerTest.cs
+++ OnlineBussen/OnlineBussen.Tests/UserControllerTest.cs
@@ -18,74 +18,9 @@ public class UserControllerTest
     }
 
     [Fact]
-    public async Task GetUserByUsernameAsync_ReturnsUser_WhenUserExists()
-    {
-        // Arrange
-        var expectedUser = new User { Username = "testuser", Password = "testpass" };
-        _mockUserService
-            .Setup(x => x.GetUserByUsernameAsync("testuser"))
-            .ReturnsAsync(expectedUser);
-
-        // Act
-        var result = await _controller.GetUserByUsernameAsync("testuser");
-
-        // Assert
-        Assert.NotNull(result);
-        Assert.Equal(expectedUser.Username, result.Username);
-    }
-
-    [Fact]
-    public async Task GetUserByUsernameAsync_ReturnsUser_WhenUserDoesNotExists()
-    {
-        // Arrange
-        var expectedUser = new User { Username = "testuser", Password = "testpass" };
-        _mockUserService
-            .Setup(x => x.GetUserByUsernameAsync("testuser"))
-            .ReturnsAsync(expectedUser);
-
-        // Act
-        var result = await _controller.GetUserByUsernameAsync("user");
-
-        // Assert
-        Assert.Null(result);
-    }
-
-    [Fact]
-    public async Task UpdateUserAsync_ReturnsError_WhenUsernameExists()
-    {
-        // Arrange
-        var user = new User { Username = "oldname", Password = "pass" };
-
-        _mockUserService
-            .Setup(x => x.UsernameExistsAsync("existingname"))
-            .ReturnsAsync(true);
-
-        // Act
-        var result = await _controller.UpdateUserAsync(user, "existingname");
-
-        // Assert
-        Assert.False(result.succes);
-        Assert.Equal("A User with that name already exists", result.message);
-    }
-
-    [Fact]
-    public async Task UpdateUserAsync_ReturnsSucces()
-    {
-        var user = new User { Username = "oldname", Password = "pass" };
-        _mockUserService
-            .Setup(x => x.UsernameExistsAsync("newname"))
-            .ReturnsAsync(false);
-
-        var result = await _controller.UpdateUserAsync(user, "newname");
-
-        Assert.True(result.succes);
-        Assert.Equal("User succesfully updated", result.message);
-    }
-
-    [Fact]
     public async Task CreateUser_ReturnsSucces()
     {
-        
+
         _mockUserService
             .Setup(x => x.UsernameExistsAsync("existinguser"))
             .ReturnsAsync(true);
@@ -129,7 +64,7 @@ public class UserControllerTest
 
     [Fact]
     public async Task AuthenticateUser_ReturnsFalse_UserDoesNotExist()
-    { 
+    {
         var user = new User { Username = "testuser", Password = "testpass" };
         _mockUserService
             .Setup(x => x.GetUserByCredentialsAsync("testuser", "testpass"))
@@ -141,28 +76,80 @@ public class UserControllerTest
     }
 
     [Fact]
-    public async Task AuthenticateUser_ReturnTrue_CorrectPassword()
+    public async Task AuthenticateUser_ReturnFalse_InCorrectPassword()
     {
         _mockUserService
             .Setup(x => x.GetUserByCredentialsAsync("testuser", "testpass"))
             .ReturnsAsync(new User { Username = "testuser", Password = "testpass" });
 
 
-        var result = await _controller.AuthenticateUserAsync("testuser", "testpass");
-
-        Assert.NotNull(result);
-    }
-
-    [Fact]
-    public async Task AuthenticateUser_ReturnFalse_InCorrectPassword()
-    { 
-        _mockUserService
-            .Setup(x => x.GetUserByCredentialsAsync("testuser", "testpass"))
-            .ReturnsAsync(new User { Username = "testuser", Password = "testpass" });
-
-        
         var result = await _controller.AuthenticateUserAsync("testuser", "testwrong");
 
         Assert.Null(result);
     }
+
+    [Fact]
+    public async Task GetUserByUsernameAsync_ReturnsUser_WhenUserExists()
+    {
+        // Arrange
+        var expectedUser = new User { Username = "testuser", Password = "testpass" };
+        _mockUserService
+            .Setup(x => x.GetUserByUsernameAsync("testuser"))
+            .ReturnsAsync(expectedUser);
+
+        // Act
+        var result = await _controller.GetUserByUsernameAsync("testuser");
+
+        // Assert
+        Assert.NotNull(result);
+        Assert.Equal(expectedUser.Username, result.Username);
+    }
+
+    [Fact]
+    public async Task GetUserByUsernameAsync_ReturnsUser_WhenUserDoesNotExists()
+    {
+        var expectedUser = new User { Username = "testuser", Password = "testpass" };
+        _mockUserService
+            .Setup(x => x.GetUserByUsernameAsync("testuser"))
+            .ReturnsAsync(expectedUser);
+
+        var result = await _controller.GetUserByUsernameAsync("user");
+
+        Assert.Null(result);
+    }
+
+    [Fact]
+    public async Task UpdateUserAsync_ReturnsError_WhenUsernameExists()
+    {
+        var user = new User { Username = "oldname", Password = "pass" };
+
+        _mockUserService
+            .Setup(x => x.UsernameExistsAsync("existingname"))
+            .ReturnsAsync(true);
+
+        var result = await _controller.UpdateUserAsync(user, "existingname");
+
+        Assert.False(result.succes);
+        Assert.Equal("A User with that name already exists", result.message);
+    }
+
+    [Fact]
+    public async Task UpdateUserAsync_ReturnsSucces()
+    {
+        var user = new User { Username = "oldname", Password = "pass" };
+        _mockUserService
+            .Setup(x => x.UsernameExistsAsync("newname"))
+            .ReturnsAsync(false);
+
+        var result = await _controller.UpdateUserAsync(user, "newname");
+
+        Assert.True(result.succes);
+        Assert.Equal("User succesfully updated", result.message);
+    }
+
+    
+
+    
+
+    
 }
